27ee2e840f25a66bdae27cf71573be27c6efa18b
Arthur Gorr <art.gorr@gmail.com>
2014-12-24 18:56:25 -0600

    add a category

--- NodejsWebApp/NodejsWebApp/data/seedData.js
+++ NodejsWebApp/NodejsWebApp/data/seedData.js
@@ -1,21 +1,32 @@
-﻿(function (seedData) {
+﻿(function(seedData) {
 
-        seedData.initialNotes = [
-            {
-                name: "History",
-                notes: [
-                    {
-                        note: "Testing History",
-                        author: "Arth G",
-                        color: "yellow"
-                    },
-                    {
-                        note: "I like History",
-                        author: "ArthG",
-                        color: "blue"
-                    }
-                ]
-            }
-        ];
+    seedData.initialNotes = [
+        {
+            name: "History",
+            notes: [
+                {
+                    note: "Testing History",
+                    author: "Arth G",
+                    color: "yellow"
+                },
+                {
+                    note: "I like History",
+                    author: "ArthG",
+                    color: "blue"
+                }
+            ]
+        },
+        {
+            name: "Orchestra",
+            notes: [
+                {
+                    note: "Testing Orchestra",
+                    author: "ArthG",
+                    color: "yellow"
+                }
+            ]
+        }
+    ];
 
-    })(module.exports);
+
+})(module.exports);
