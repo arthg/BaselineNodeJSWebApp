4e346734f77b5c5032a56dbd6ae9b3f065d79a39
Arthur Gorr <art.gorr@gmail.com>
2014-12-24 18:24:09 -0600

    Add require; add sample code for Express+Vash Does not run

--- NodejsWebApp/NodejsWebApp/server.js
+++ NodejsWebApp/NodejsWebApp/server.js
@@ -1,6 +1,29 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿var http = require("http");
+var express = require("express");
+var app = express();
+
+var controllers = require("./controllers");
+
+app.set("view engine", "vash"); // vash view engine
+
+// set the public static resource folder
+app.use(express.static(__dirname + "/public"));
+
+
+app.get("/", function (req, res) {
+    res.render("index", { title: "Express + Vash" });
+});
+
+app.get("/api/users", function (req, res) {
+    res.set("Content-Type", "application/json");
+    res.send({
+        name: "Art",
+        isValid: false,
+        group: "Adminisfuckinstartor"
+    });
+});
+
+var server = http.createServer(app);
+
+
+server.listen(3000);
